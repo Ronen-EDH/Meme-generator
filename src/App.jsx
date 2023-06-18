47d62a52bf560842bf9341f2de4ee7b6c64fe611
David Horvath <calogero.c.23@gmail.com>
2023-06-18 18:13:46 +0100

    Added the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import "./App.scss";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>Hello Bello</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <>
+      <header className="header">
+        <img className="header__image" src="..\src\assets\troll-face-png-19697.png" alt="troll-face icon" />
+        <h2 className="header__title">Meme Generator</h2>
+        <h4 className="header__project">React Course - Project 3</h4>
+      </header>
+    </>
+  );
+}
