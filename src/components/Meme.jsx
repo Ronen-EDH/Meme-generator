f0cd1cf2f7df5d606fbea5de5157555380994439
David Horvath <calogero.c.23@gmail.com>
2023-06-19 15:53:47 +0100

    Getting a random meme from an array

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,23 @@
+import memesData from "../memesData";
+
 export function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    // console.log(randomNumber);
+    console.log(url);
+  }
+
   return (
     <main className="main">
-      <form className="main__form-container">
+      <div className="main__form-container">
         <input className="main__form main__form-text" type="text" placeholder="Top text" />
         <input className="main__form main__form-text" type="text" placeholder="Bottom text" />
-        <button className="main__form main__form-button">Get a new form image 🖼</button>
-      </form>
+        <button onClick={getMemeImage} className="main__form main__form-button">
+          Get a new form image 🖼
+        </button>
+      </div>
     </main>
   );
 }
