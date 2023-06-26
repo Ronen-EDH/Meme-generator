3e22623669d934eb6f84297ef1f4f80ed33d0d85
David Horvath <calogero.c.23@gmail.com>
2023-06-26 19:30:58 +0100

    Button works with random meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,14 @@
 import memesData from "../memesData";
+import { useState } from "react";
 
 export function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
-    // console.log(randomNumber);
-    console.log(url);
+    setMemeImage(url);
   }
 
   return (
@@ -15,9 +17,10 @@ export function Meme() {
         <input className="main__form main__form-text" type="text" placeholder="Top text" />
         <input className="main__form main__form-text" type="text" placeholder="Bottom text" />
         <button onClick={getMemeImage} className="main__form main__form-button">
-          Get a new form image 🖼
+          Get a new meme image 🖼
         </button>
       </div>
+      <img className="main__memeImg" src={memeImage} alt="Meme Image" />
     </main>
   );
 }
